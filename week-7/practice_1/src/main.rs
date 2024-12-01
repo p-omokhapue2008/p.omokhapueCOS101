e0181a4616d54d44b870a16d8ab1bf16b6123783
p-omokhapue2008 <peculiaromokhapue165@gmail.com>
2024-12-01 23:29:12 +0100

    (My sixth commit)

---
+++ week-7/practice_1/src/main.rs
@@ -0,0 +1,11 @@
+fn my_grade() {
+
+    //function body
+    println!("Greetings from function my grade!");
+}
+
+fn main(){
+
+    //calling a function
+    my_grade();
+}
---
+++ week-7/practice_9/target/debug/practice_9.d
@@ -0,0 +1,1 @@
+C:\Users\JOE\ OMOKHAPUE\Documents\p.omokhapueCOS101\week-7\practice_9\target\debug\practice_9.exe: C:\Users\JOE\ OMOKHAPUE\Documents\p.omokhapueCOS101\week-7\practice_9\src\main.rs
